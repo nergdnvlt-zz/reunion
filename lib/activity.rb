eb745d32659f475628187780e9a63bb522a15a91
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-21 10:02:50 -0700

    Working on cost difference.

--- lib/activity.rb
+++ lib/activity.rb
@@ -4,14 +4,26 @@ class Activity
   def initialize
     @participants = []
     @cost_array = []
+    @participant_array = []
   end
 
   def add_participant(participant)
     @participants << participant[:name]
     @cost_array << participant[:paid]
+    @participant_array << participant
   end
 
   def total_cost
     @cost_array.inject(0, :+)
+  end
+
+  def split_cost
+    total_cost / @participants.count
+  end
+
+  def cost_difference(name_given)
+    @participant_array.map do |participant|
+      return split_cost - participant[:paid] if participant[:name] == name_given
+    end
   end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,23 +4,26 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def setup
+    @hiking = Activity.new
+    @hiking.add_participant({ name: 'Odin', paid: 20.00 })
+    @hiking.add_participant({ name: 'Thor', paid: 30.00 })
+    @hiking.add_participant({ name: 'Bob', paid: 10.00 })
+  end
+
   def test_create_instance_of_activity
-    hiking = Activity.new
-    assert_instance_of Activity, hiking
+    assert_instance_of Activity, @hiking
   end
 
   def test_activity_has_a_participant
     hiking = Activity.new
     hiking.add_participant({ name: 'Bob', paid: 20.00 })
+
     assert_equal ['Bob'], hiking.participants
   end
 
   def test_add_multiple_participants
-    hiking = Activity.new
-    hiking.add_participant({ name: 'Odin', paid: 20.00 })
-    hiking.add_participant({ name: 'Thor', paid: 20.00 })
-    hiking.add_participant({ name: 'Bob', paid: 20.00 })
-    assert_equal ['Odin', 'Thor', 'Bob'], hiking.participants
+    assert_equal ['Odin', 'Thor', 'Bob'], @hiking.participants
   end
 
   def test_total_cost_one_participant
@@ -31,10 +34,24 @@ class ActivityTest < Minitest::Test
   end
 
   def test_total_cost_multiple_participants
-    hiking = Activity.new
-    hiking.add_participant({ name: 'Odin', paid: 20.00 })
-    hiking.add_participant({ name: 'Thor', paid: 30.00 })
-    hiking.add_participant({ name: 'Bob', paid: 10.00 })
-    assert_equal 60, hiking.total_cost
+    assert_equal 60, @hiking.total_cost
+  end
+
+  def test_split_cost
+    assert_equal 20.00, @hiking.split_cost
+  end
+
+  def test_cost_difference
+    result = @hiking.cost_difference('Bob')
+    assert_equal 10.0, result
+  end
+
+  def test_cost_difference_all
+    result = @hiking.cost_difference('Bob')
+    assert_equal 10.0, result
+    result2 = @hiking.cost_difference('Odin')
+    assert_equal 0.0, result2
+    result3 = @hiking.cost_difference('Thor')
+    assert_equal (-10.0), result3
   end
 end
