6d8bf63f77819fecb2d079d6960301225604d52a
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-21 09:42:55 -0700

    Working on first iteration.

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,17 @@
+# Creates an activity instance
 class Activity
+  attr_reader :participants
+  def initialize
+    @participants = []
+    @cost_array = []
+  end
+
+  def add_participant(participant)
+    @participants << participant[:name]
+    @cost_array << participant[:paid]
+  end
+
+  def total_cost
+    @cost_array.inject(0, :+)
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,37 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_create_instance_of_activity
+    hiking = Activity.new
+    assert_instance_of Activity, hiking
+  end
+
+  def test_activity_has_a_participant
+    hiking = Activity.new
+    hiking.add_participant({ name: 'Bob', paid: 20.00 })
+    assert_equal ['Bob'], hiking.participants
+  end
+
+  def test_add_multiple_participants
+    hiking = Activity.new
+    hiking.add_participant({ name: 'Odin', paid: 20.00 })
+    hiking.add_participant({ name: 'Thor', paid: 20.00 })
+    hiking.add_participant({ name: 'Bob', paid: 20.00 })
+    assert_equal ['Odin', 'Thor', 'Bob'], hiking.participants
+  end
+
+  def test_total_cost_one_participant
+    hiking = Activity.new
+    hiking.add_participant({ name: 'Bob', paid: 20.00 })
+
+    assert_equal 20.00, hiking.total_cost
+  end
+
+  def test_total_cost_multiple_participants
+    hiking = Activity.new
+    hiking.add_participant({ name: 'Odin', paid: 20.00 })
+    hiking.add_participant({ name: 'Thor', paid: 30.00 })
+    hiking.add_participant({ name: 'Bob', paid: 10.00 })
+    assert_equal 60, hiking.total_cost
+  end
 end
